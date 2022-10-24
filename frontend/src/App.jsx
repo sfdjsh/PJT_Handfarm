e254c159bfc773a4bbfe4979ccaf31684527d685
김강현 <kiki249049@gmail.com>
2022-10-24 11:53:03 +0900

    settings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,27 @@
 import './App.css';
 import logo from './HandFarm.png';
-// import {Button} from "@mui/material"
+import {Button} from "@mui/material"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+import Login from "./pages/auth/Login";
+import GlobalStyle from "./style/GlobalStyle";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img className='App-logo' src={ logo } alt="HandFarm" />
-        <div>
-          <p>원격으로 농장 관리와,</p>
-          <p>커뮤니티 기능으로,</p>
-          <p>나의 농장을 관리해보세요!</p>
-        </div>
-      </header>
+    <>
+      <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route exact path='/login' element={<Login/>} />
+        </Routes>
+      </Router>
       {/* <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button> */}
-    </div>
+    </>
   )
 };
 
