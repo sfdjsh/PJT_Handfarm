ce2ca45350e56d074a7f76d1c99b1ffc2e62d668
김혜진 <aa981204@naver.com>
2022-10-24 17:50:59 +0900

    #4 feat : farmmunity 수정

--- backend/src/main/java/com/handfarm/backend/controller/FarmmunityController.java
+++ backend/src/main/java/com/handfarm/backend/controller/FarmmunityController.java
@@ -3,17 +3,22 @@ package com.handfarm.backend.controller;
 import com.handfarm.backend.domain.dto.article.ArticleRegistDto;
 import com.handfarm.backend.service.FarmmunityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/community")
 public class FarmmunityController {
-
+    private static final String success = "SUCCESS";
+    private static final String fail = "FAIL";
+    private static HttpStatus status = HttpStatus.NOT_FOUND; // 404에러
     private FarmmunityService farmmunityService;
 
     @Autowired
@@ -23,7 +28,10 @@ public class FarmmunityController {
 
     @PostMapping("/{domain}/{category}")
     public ResponseEntity<?> registArticle(HttpServletRequest request, ArticleRegistDto articleRegistDto){
+        Map<String, Object> resultMap = new HashMap<>();
         farmmunityService.registArticle(articleRegistDto);
+
+        return new ResponseEntity<>(resultMap, status);
     }
 
 }
--- backend/src/main/java/com/handfarm/backend/service/UserService.java
+++ backend/src/main/java/com/handfarm/backend/service/UserService.java
@@ -4,7 +4,4 @@ package com.handfarm.backend.service;
 import java.util.Map;
 
 public interface UserService {
-    String[] getKakaoAccessToken(String code);
-
-    Map<String,Object> createKakaoUser(String access_token);
 }
