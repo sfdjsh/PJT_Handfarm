02605e09ba5dc0360187ffefc7055aaf75f61764
김강현 <kiki249@naver.com>
2022-10-24 17:48:42 +0900

    # 5 feat : 파뮤니티 화면 구현

---
+++ frontend/src/config.js
@@ -0,0 +1,1 @@
+export const LOCAL_URL = "https://localhost:8080/api"
--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Login = () => {
     return (
         <div>
-            하이
+            하이 안녕하세요
         </div>
     );
 };
