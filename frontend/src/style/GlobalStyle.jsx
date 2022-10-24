5a34405100a56d6a96185cd543d8e8709d9b53f2
김강현 <kiki249@naver.com>
2022-10-24 17:48:42 +0900

    # 5 feat : 파뮤니티 화면 구현

--- frontend/src/style/GlobalStyle.jsx
+++ frontend/src/style/GlobalStyle.jsx
@@ -3,6 +3,7 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
   body {
     font-family: SCoreDream;
+    background-color: white;
   }
 `
 export default GlobalStyle
