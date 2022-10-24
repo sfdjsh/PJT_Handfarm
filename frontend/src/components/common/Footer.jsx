9ebf4ddccf89379c95f2e08572e882d01ab736c2
김강현 <kiki249049@gmail.com>
2022-10-24 15:09:13 +0900

    temp

---
+++ frontend/src/components/common/Footer.jsx
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined';
+import MailOutlineOutlinedIcon from '@mui/icons-material/MailOutlineOutlined';
+import GiteOutlinedIcon from '@mui/icons-material/GiteOutlined';
+import styled from 'styled-components';
+import Divider from "@mui/material/Divider";
+import { makeStyles } from '@mui/styles';
+
+// export const footer = styled.div`
+//     height : 100%;
+//     font-family: "New Gulim";
+// `
+const useStyles = makeStyles({
+    root: {
+        width: "100vw",
+        fontFamily : "ScoreDream"
+    },
+});
+
+export const Footer = () => {
+    const classes = useStyles();
+    const [value, setValue] = useState(0)
+
+    return (
+        <>
+            <Box style={{ width : "100vw", position : "fixed", bottom : 0 }}>
+                <Divider style={{ backgroundColor : "white" }}/>
+                <BottomNavigation
+                    showLabels
+                    value={value}
+                    onChange={(event, newValue) => {
+                        setValue(newValue);
+                    }}
+                    className={classes.root}
+                    style={{ backgroundColor : "black" }}
+                >
+                    <BottomNavigationAction style={{ color : "white" }}  label="농장" icon={<GiteOutlinedIcon />} />
+                    <BottomNavigationAction style={{ color : "white" }}  label="커뮤니티" icon={<ChatBubbleOutlineIcon />} />
+                    <BottomNavigationAction style={{ color : "white" }}  label="쪽지함" icon={<MailOutlineOutlinedIcon />} />
+                    <BottomNavigationAction style={{ color : "white" }}  label="프로필" icon={<PermIdentityOutlinedIcon />} />
+                </BottomNavigation>
+            </Box>
+        </>
+    );
+};
+
+export default Footer;
--- frontend/src/style/GlobalStyle.jsx
+++ frontend/src/style/GlobalStyle.jsx
@@ -2,7 +2,9 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
-    font-family: SCoreDream;
+    font-family: ScoreDream;
+    background-color: #000000;
+    color : white;
   }
 `
 export default GlobalStyle
