1cc2cec801d249b48dcf8d61572877bb00b063bd
정석호 <sfdjsh@naver.com>
2022-10-25 13:36:21 +0900

    Feat : 카카오톡 로그인 구현

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import {KAKAO_AUTH_URL} from './OAuth';
 
 export const Login = () => {
     return (
-        <div>
-            하이 안녕하세요
-        </div>
+        <>
+            <img src="handFarm.png" alt="핸드팜" 
+            style={{width:'70vmin', height:'30vmin', paddingTop:'100px'}}/>
+            <div className='Main-word'>
+                <h3 style={{color:'#F24822'}}>원격으로 농장 관리와,</h3>
+                <h3 style={{color:'#0D99FF'}}>커뮤니티 기능으로,</h3>
+                <h3 style={{color:'#9747FF'}}>나의 농장
+                    <span style={{color:'white'}}>을 관리해보세요!</span>
+                </h3>
+            </div>
+            <button><a href={KAKAO_AUTH_URL}>카카오톡 로그인</a></button>
+        </>
     );
 };
 
--- frontend/src/pages/community/CommunityMain.jsx
+++ frontend/src/pages/community/CommunityMain.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import axios from 'axios'
+import {LOCAL_URL} from "../../config";
 
 export const CommunityMain = () => {
+    const href = window.location.href;
+    let params = new URL(document.URL).searchParams
+    let code = params.get("code")
+    console.log(code)
+  
+    // const [user, Setuser] = useState({
+    //     userinfo: '',
+    //     accessToken: '',
+    //     refreshToken: '',
+    //     isRegisted: null
+    // });
+  
+    useEffect(async () => { 
+    //   axios({
+    //     method: 'GET',
+    //     url: `${LOCAL_URL}/api/kakao`,
+    //     Headers: {code}
+    //   })
+    //     .then(response => {
+    //         console.log(response.data)
+    //     })
+    })
+
     return (
         <div>
-
+            <h1>토큰 받아와지나??</h1>
         </div>
     );
 };
