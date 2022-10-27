1bea9fce2498e0ba85c6a6aa99d44845bc8b9055
정석호 <sfdjsh@naver.com>
2022-10-27 17:59:53 +0900

    App.jsx 푸시

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import './App.css';
-import logo from './HandFarm.png';
-import {Button} from "@mui/material"
 import {
   BrowserRouter as Router,
   Routes,
@@ -8,16 +6,11 @@ import {
 } from "react-router-dom"
 import Login from "./pages/auth/Login";
 import GlobalStyle from "./style/GlobalStyle";
+import Header from "./components/common/Header"
 import Footer from "./components/common/Footer";
-import styled from 'styled-components';
-import Box from '@mui/material/Box';
-import Divider from '@mui/material/Divider';
 import ArticleForm from "./pages/community/ArticleForm";
 import CommunityMain from "./pages/community/CommunityMain"
-import Navbar from "./components/common/Navbar";
-import FarmmunityMain from "./pages/community/FarmmunityMain";
-import Mqtt from "./mqtt/Mqtt";
-// import Mqtt2 from "./mqtt/Mqtt2";
+
 //
 // export const Wrapper = styled.div`
 //   height : auto;
@@ -28,16 +21,17 @@ function App() {
   return (
     <>
       <GlobalStyle />
+        <Header />
           <Router>
               <Navbar/>
             <Routes>
               <Route exact path='/' element={<Login/>} />
               <Route exact path='/community' element={<FarmmunityMain />}></Route>
               <Route exact path='/community/create' element={<ArticleForm/>} />
-                <Route exact path='/mqtt' element={<Mqtt/>} />
             </Routes>
             <Footer/>
           </Router>
+        <Footer/>
     </>
   )
 };
