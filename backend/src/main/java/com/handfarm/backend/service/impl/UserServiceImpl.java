b8c6a6b0798278dbb95191e82fc5463925908ee1
유승우 <swyou1123@naver.com>
2022-10-28 13:26:26 +0900

    F03 be 유승우

--- backend/src/main/java/com/handfarm/backend/service/impl/UserServiceImpl.java
+++ backend/src/main/java/com/handfarm/backend/service/impl/UserServiceImpl.java
@@ -1,18 +1,11 @@
 package com.handfarm.backend.service.impl;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 import com.handfarm.backend.domain.entity.UserEntity;
 import com.handfarm.backend.repository.UserRepository;
 import com.handfarm.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 
 @Service
