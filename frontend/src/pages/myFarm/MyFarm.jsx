f72c127824386d36977157c2a24ac1a2fc501b34
정석호 <sfdjsh@naver.com>
2022-10-28 17:57:27 +0900

    카카오톡 로그인 미완성

--- frontend/src/pages/myFarm/MyFarm.jsx
+++ frontend/src/pages/myFarm/MyFarm.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import Alarm from "../api/Alarm";
+import {LOCAL_URL} from "../../config";
 
 const MyFarm = () => {
+  const URL = `${LOCAL_URL}/notice/count`
+  const accessToken = localStorage.getItem('access_token')
+
+  // useEffect(() => {
+  //   axios(URL, {
+  //     method: "GET",
+  //     headers : {
+  //       'Authorization' : accessToken
+  //     }
+  //   })
+  //     .then(response => {console.log(response)})
+  // }, [])
+
   return (
     <div>
       내 농장 페이지임
+      <button>
+        {/* 알람 들어옴?? */}
+      </button>
     </div>
   )
 };
