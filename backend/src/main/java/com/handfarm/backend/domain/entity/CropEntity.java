af359b01e6b7dfd7a39b8f3ca2f8f4251182d38b
김혜진 <aa981204@naver.com>
2022-10-28 14:41:08 +0900

    F00 00 be 김혜진

--- backend/src/main/java/com/handfarm/backend/domain/entity/CropEntity.java
+++ backend/src/main/java/com/handfarm/backend/domain/entity/CropEntity.java
@@ -2,6 +2,7 @@ package com.handfarm.backend.domain.entity;
 
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
@@ -13,6 +14,7 @@ import javax.persistence.*;
 @Setter
 @DynamicUpdate
 @DynamicInsert
+@NoArgsConstructor
 @Table(name="crops")
 public class CropEntity {
 
