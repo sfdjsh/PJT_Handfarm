63484bda903db4159e17e5dfb788f5c16ec2517d
유승우 <swyou1123@naver.com>
2022-10-28 13:26:26 +0900

    F03 be 유승우

---
+++ backend/src/main/java/com/handfarm/backend/domain/entity/DeviceEntity.java
@@ -0,0 +1,46 @@
+package com.handfarm.backend.domain.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@Table(name="devices")
+public class DeviceEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idx;
+
+    @Column(name = "device_no")
+    private String deviceNo;
+
+    @Column(name = "device_img")
+    private String deviceImg;
+
+    @Column(name="device_location")
+    private Float deviceLoation;
+
+    @Column(name="device_latitude")
+    private Float deviceLatitude;
+
+    @Column(name="device_long")
+    private Integer deviceLong;
+
+    @Column(name="device_name")
+    private String deviceName;
+
+    @ManyToOne
+    @JoinColumn(name="device_crops")
+    private CropEntity CropsIdx;
+
+    @Builder
+    private DeviceEntity(String deviceNo, CropEntity deviceCrops){
+        this.deviceNo = deviceNo;
+        this.CropsIdx = deviceCrops;
+    }
+}
---
+++ backend/src/main/java/com/handfarm/backend/service/impl/DeviceServiceImpl.java
@@ -0,0 +1,75 @@
+package com.handfarm.backend.service.impl;
+
+import com.google.gson.JsonElement;
+import com.handfarm.backend.domain.dto.device.DeviceRegistDto;
+import com.handfarm.backend.domain.entity.DeviceEntity;
+import com.handfarm.backend.domain.entity.UserEntity;
+import com.handfarm.backend.repository.CropRepository;
+import com.handfarm.backend.repository.DeviceRepository;
+import com.handfarm.backend.repository.UserRepository;
+import com.handfarm.backend.service.DeviceService;
+import com.handfarm.backend.service.KakaoService;
+import com.handfarm.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Optional;
+
+@Service
+public class DeviceServiceImpl implements DeviceService {
+
+
+
+
+    DeviceRepository deviceRepository;
+
+    @Autowired
+    DeviceServiceImpl(DeviceRepository deviceRepository){this.deviceRepository= deviceRepository;}
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    KakaoService kakaoService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    CropRepository cropRepository;
+
+    @Override
+    public void registDevice(DeviceRegistDto deviceRegistDto) throws IOException {
+        DeviceEntity deviceEntity = DeviceEntity.builder()
+                .deviceCrops(cropRepository.findByCropName(deviceRegistDto.getDeviceCrops()))
+                .deviceNo(deviceRegistDto.getDeviceNo())
+                .build();
+        deviceRepository.save(deviceEntity);
+    }
+
+    @Override
+    public String userRegistDevice(HttpServletRequest request, DeviceRegistDto deviceRegistDto) throws IOException {
+        try{
+            JsonElement element = kakaoService.CheckAccessToken(request.getHeader("accesstoken"));
+            String email = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("email").getAsString();
+
+            Optional<DeviceEntity> deviceEntity = deviceRepository.findByDeviceNo(deviceRegistDto.getDeviceNo());
+            deviceEntity.get().setDeviceName(deviceRegistDto.getDeviceName());
+            deviceEntity.get().setCropsIdx(cropRepository.findByCropName(deviceRegistDto.getDeviceCrops()));
+            deviceRepository.save(deviceEntity.get());
+
+            Optional<UserEntity> userEntity = userRepository.findByUserId(email);
+            userEntity.get().setUserDevice(deviceEntity.get());
+
+           userRepository.save(userEntity.get());
+
+        }catch (IOException e){
+            e.printStackTrace();
+            return "timeOut";
+        }
+        return "sucess";
+    }
+
+}
