2afade6583a8d78de3c47959723f6e526dea0de2
유승우 <swyou1123@naver.com>
2022-10-28 14:43:20 +0900

    2022-10-28 commit

--- backend/src/main/java/com/handfarm/backend/domain/entity/SensorEntity.java
+++ backend/src/main/java/com/handfarm/backend/domain/entity/SensorEntity.java
@@ -1,4 +1,28 @@
 package com.handfarm.backend.domain.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@DynamicUpdate
+@DynamicInsert
+@NoArgsConstructor
+@Table(name="sensor")
 public class SensorEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idx;
+
+    @Column(name="sensor_name")
+    private String sensorName;
+
+    @Column(name = "sensor_area")
+    private String sensorArea;
 }
