37bb041c2bb989aca04a9b0c8e6b51c5a43f8e18
정석호 <sfdjsh@naver.com>
2022-10-28 12:09:19 +0900

    Feat : My Farm 등록 폼 및 렌더링

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { AppBar, IconButton, Divider, Box } from '@mui/material'
 import NotificationsIcon from '@mui/icons-material/NotificationsNone';
 import { display } from '@mui/system';
 
 export const Header = () => {
+
   return (
     <>
       <Box sx={{ display:"flex", justifyContent:"end", alignItems:"center" }}>
