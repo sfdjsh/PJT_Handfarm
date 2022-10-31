da41290d416c873601b87c9e07008167252d92aa
유승우 <swyou1123@naver.com>
2022-10-31 16:44:46 +0900

    2022-10-31 commit

--- backend/src/main/java/com/handfarm/backend/service/impl/DeviceServiceImpl.java
+++ backend/src/main/java/com/handfarm/backend/service/impl/DeviceServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
@@ -54,6 +55,9 @@ public class DeviceServiceImpl implements DeviceService {
         try{
             JsonElement element = kakaoService.CheckAccessToken(request.getHeader("accessToken"));
             String email = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("email").getAsString();
+            System.out.println(email);
+            System.out.println(deviceRegistDto);
+            System.out.println(deviceRegistDto.getDeviceNo());
 
             Optional<DeviceEntity> deviceEntity = deviceRepository.findByDeviceNo(deviceRegistDto.getDeviceNo());
             deviceEntity.get().setDeviceName(deviceRegistDto.getDeviceName());
@@ -63,13 +67,26 @@ public class DeviceServiceImpl implements DeviceService {
             Optional<UserEntity> userEntity = userRepository.findByUserId(email);
             userEntity.get().setDevice(deviceEntity.get());
 
-           userRepository.save(userEntity.get());
+            userRepository.save(userEntity.get());
 
         }catch (IOException e){
             e.printStackTrace();
             return "timeOut";
         }
         return "sucess";
+    }
+
+    @Override
+    public Boolean deviceUpdate(HttpServletRequest request, DeviceRegistDto deviceRegistDto){
+        try {
+            Optional<DeviceEntity> deviceEntity = deviceRepository.findByDeviceNo(deviceRegistDto.getDeviceNo());
+            deviceEntity.get().setDeviceName(deviceRegistDto.getDeviceName());
+            deviceEntity.get().setCrop(cropRepository.findByCropName(deviceRegistDto.getDeviceCrops()));
+            deviceRepository.save(deviceEntity.get());
+            return true;
+        } catch (NoSuchElementException e){
+            return false;
+        }
     }
 
 }
