7c6bf3265622631743e88b0c3b031e234e2de3bc
김도원 <da9065@naver.com>
2022-10-31 18:02:01 +0900

    air update

--- ESP32s/Sensor_Test_1/Sensor_Test_1/Sensor_Test_1.ino
+++ ESP32s/Sensor_Test_1/Sensor_Test_1/Sensor_Test_1.ino
@@ -1,7 +1,5 @@
 #include "getTempHumid.h"
 
-#include <Wire.h>
-
 void setup() {
   Serial.begin(115200);
 }
@@ -9,34 +7,10 @@ void setup() {
 void loop() {
   // float* TempHumidArr = getTempHumid();           //get Temp, Humid | TempHumidArr[0] = Temp, TempHumidArr[1] = Humid;
   // Serial.println(getCDS());                       //get CDS Value
-   byte error, address;
-  int nDevices;
-  Serial.println("Scanning...");
-  nDevices = 0;
-  for(address = 1; address < 127; address++ ) {
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
-    if (error == 0) {
-      Serial.print("I2C device found at address 0x");
-      if (address<16) {
-        Serial.print("0");
-      }
-      Serial.println(address,HEX);
-      nDevices++;
-    }
-    else if (error==4) {
-      Serial.print("Unknow error at address 0x");
-      if (address<16) {
-        Serial.print("0");
-      }
-      Serial.println(address,HEX);
-    }    
-  }
-  if (nDevices == 0) {
-    Serial.println("No I2C devices found\n");
-  }
-  else {
-    Serial.println("done\n");
-  }
-  delay(5000);          
+  float* pm_arr = get_pm();
+  Serial.print("Number of 2.5 um :");
+  Serial.println(pm_arr[0]);
+  Serial.print("Number of 10 um : ");
+  Serial.println(pm_arr[1]);
 }
+
