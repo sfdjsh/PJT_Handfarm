f7783554d60f53270f33116389881e5c2ed618ff
김혜진 <aa981204@naver.com>
2022-10-31 13:29:49 +0900

    #8. feat : ChattRedisRepository 생성

---
+++ backend/src/main/java/com/handfarm/backend/domain/entity/ChattInfoEntity.java
@@ -0,0 +1,25 @@
+package com.handfarm.backend.domain.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+
+@Getter
+@NoArgsConstructor
+@RedisHash(value = "ChattInfo") // Redis의 keyspace값
+public class ChattInfoEntity {
+    @Id
+    private Integer idx;
+    private String personA;
+    private String personB;
+    private Integer roomId;
+
+    public ChattInfoEntity(Integer idx, String personA, String personB, Integer roomId) {
+        this.idx = idx;
+        this.personA = personA;
+        this.personB = personB;
+        this.roomId = roomId;
+    }
+}
