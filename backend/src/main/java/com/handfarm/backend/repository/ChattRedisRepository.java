4ad8a73800ebc930e31b1d0169faa67174e03e62
김혜진 <aa981204@naver.com>
2022-11-01 13:21:52 +0900

    #8. feat : ChattRedisRepository 생성

--- backend/src/main/java/com/handfarm/backend/repository/ChattRedisRepository.java
+++
@@ -1,7 +0,0 @@
-package com.handfarm.backend.repository;
-
-import com.handfarm.backend.domain.entity.ChattEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ChattRedisRepository extends CrudRepository<ChattEntity, Integer> {
-}
