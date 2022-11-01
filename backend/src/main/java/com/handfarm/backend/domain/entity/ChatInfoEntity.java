6cd7a8ee5fcb89fa21fb2f1e2bdcfb147568f373
김혜진 <aa981204@naver.com>
2022-11-01 17:56:14 +0900

    #8. feat : Chat-Redis 구현

--- backend/src/main/java/com/handfarm/backend/domain/entity/ChatInfoEntity.java
+++ backend/src/main/java/com/handfarm/backend/domain/entity/ChatInfoEntity.java
@@ -2,24 +2,36 @@ package com.handfarm.backend.domain.entity;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 import org.springframework.data.redis.core.RedisHash;
 
+import javax.persistence.*;
 
+
+@Entity
 @Getter
+@Setter
 @NoArgsConstructor
-@RedisHash(value = "ChattInfo") // Redis의 keyspace값
+@DynamicInsert
+@DynamicUpdate
+@Table(name="chat_infos")
 public class ChatInfoEntity {
     @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Integer idx;
-    private String personA;
-    private String personB;
-    private Integer roomId;
 
-    public ChatInfoEntity(Integer idx, String personA, String personB, Integer roomId) {
-        this.idx = idx;
+    @ManyToOne
+    @JoinColumn(name="person_a_idx")
+    private UserEntity personA;
+
+    @ManyToOne
+    @JoinColumn(name="person_b_idx")
+    private UserEntity personB;
+
+    public ChatInfoEntity(UserEntity personA, UserEntity personB) {
         this.personA = personA;
         this.personB = personB;
-        this.roomId = roomId;
     }
 }
