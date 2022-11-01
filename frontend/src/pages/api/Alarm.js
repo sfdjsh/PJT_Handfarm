592a9909b0f3aa8dc52034b3ed85c5df9e35c847
정석호 <sfdjsh@naver.com>
2022-11-01 10:21:52 +0900

    F03 fe 정석호

---
+++ frontend/src/pages/api/Alarm.js
@@ -0,0 +1,26 @@
+// import { LOCAL_URL } from "../../config";
+// import axios from 'axios'
+
+// export async function Alarm() {
+//   const URL = `${LOCAL_URL}/notice/count`
+//   const accessToken = localStorage.getItem('access_token')
+
+//   const test = await axios(URL, {
+//     method: "GET",
+//     headers: {
+//       Authorization: accessToken
+//     }
+//   }
+//     .then(response => {
+//       console.log('여기 들어옴???')
+      
+//     }))
+
+//   return (
+//     <div>
+//       <h1>알람 들어옴??</h1>
+//     </div>
+//   )
+// }
+
+// export default Alarm
