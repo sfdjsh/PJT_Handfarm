46db8662d117f21d8fd03f7bb75f4e8fe50cc75a
유승우 <swyou1123@naver.com>
2022-11-02 16:28:27 +0900

    2022-11-1 commit

--- backend/src/main/java/com/handfarm/backend/config/MqttConfig.java
+++ backend/src/main/java/com/handfarm/backend/config/MqttConfig.java
@@ -7,21 +7,84 @@ import org.eclipse.paho.client.mqttv3.MqttAsyncClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.core.MessageProducer;
 import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
 import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
 import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
 import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
 import org.springframework.integration.mqtt.support.MqttHeaders;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
 import org.springframework.messaging.handler.annotation.Header;
 
 @Configuration
+@IntegrationComponentScan
 public class MqttConfig {
+
+    public MqttPahoClientFactory mqttClientFactory() {
+        DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
+        MqttConnectOptions options = new MqttConnectOptions();
+
+        options.setServerURIs(new String[] {"tcp://54.180.201.1:1883"});
+        options.setUserName("");
+        String pass ="";
+        options.setPassword(pass.toCharArray());
+        options.setCleanSession(true);
+
+        factory.setConnectionOptions(options);
+
+        return factory;
+    }
+
+    @Bean
+    public MessageChannel mqttInputChannel() {
+        return new DirectChannel();
+    }
+    @Bean
+    public MessageProducer inbound(){
+        MqttPahoMessageDrivenChannelAdapter adapter = new MqttPahoMessageDrivenChannelAdapter("serverIn",
+                mqttClientFactory(),TOPIC_FILTER);
+        adapter.setCompletionTimeout(5000);
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(2);
+        adapter.setOutputChannel(mqttInputChannel());
+        return adapter;
+    }
+    @Bean
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    public MessageHandler handler(){
+        return new MessageHandler() {
+            @Override
+            public void handleMessage(Message<?> message) throws MessagingException {
+                String topic = message.getHeaders().get(MqttHeaders.RECEIVED_TOPIC).toString();
+                if(topic.equals(TOPIC_FILTER)){
+                    System.out.println("this is out topic");
+                }
+                System.out.println(message.getPayload());
+            }
+        };
+    }
+    @Bean
+    public MessageChannel mqttOutboundChannel(){
+        return new DirectChannel();
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "mqttOutboundChannel")
+    public MessageHandler mqttOutbound(){
+        MqttPahoMessageHandler messageHandler = new MqttPahoMessageHandler("serverOut", mqttClientFactory());
+
+        messageHandler.setAsync(true);
+        messageHandler.setDefaultTopic(TOPIC_FILTER);
+        return messageHandler;
+    }
 
     static String deviceId = "D30";
     private static final String BROKER_URL = "tcp://54.180.201.1:1883";
@@ -31,65 +94,5 @@ public class MqttConfig {
     private static final String MQTT_USERNAME = "";
     private static final String MQTT_PASSWORD = "";
 
-    private MqttConnectOptions connectOptions() {
-        MqttConnectOptions options = new MqttConnectOptions();
-        options.setCleanSession(true);
-        options.setUserName(MQTT_USERNAME);
-        options.setPassword(MQTT_PASSWORD.toCharArray());
-        return options;
-    }
 
-    @Bean
-    public DefaultMqttPahoClientFactory defaultMqttPahoClientFactory() {
-        DefaultMqttPahoClientFactory clientFactory = new DefaultMqttPahoClientFactory();
-        clientFactory.setConnectionOptions(connectOptions());
-        return clientFactory;
-    }
-    @Bean
-    public MessageChannel mqttInputChannel() {
-        return new DirectChannel();
-    }
-
-    @Bean
-    public MessageProducer inboundChannel() {
-        MqttPahoMessageDrivenChannelAdapter adapter =
-                new MqttPahoMessageDrivenChannelAdapter(BROKER_URL, MQTT_CLIENT_ID, TOPIC_FILTER);
-        adapter.setCompletionTimeout(5000);
-        adapter.setConverter(new DefaultPahoMessageConverter());
-        adapter.setQos(1);
-        adapter.setOutputChannel(mqttInputChannel());
-        return adapter;
-    }
-
-    @Bean
-    @ServiceActivator(inputChannel = "mqttInputChannel")
-    public MessageHandler inboundMessageHandler() {
-        return message -> {
-            String topic = (String) message.getHeaders().get(MqttHeaders.RECEIVED_TOPIC);
-            System.out.println("Topic:" + topic);
-            System.out.println("Payload : " + message.getPayload());
-            JsonParser parser = new JsonParser();
-            System.out.println(parser.parse((String) message.getPayload()));
-        };
-    }
-
-    @Bean
-    public MessageChannel mqttOutboundChannel() {
-        return new DirectChannel();
-    }
-
-    @Bean
-    @ServiceActivator(inputChannel = "mqttOutboundChannel")
-    public MessageHandler mqttOutbound(DefaultMqttPahoClientFactory clientFactory) {
-        MqttPahoMessageHandler messageHandler =
-                new MqttPahoMessageHandler(MQTT_CLIENT_ID, clientFactory);
-        messageHandler.setAsync(true);
-        messageHandler.setDefaultQos(1);
-        return messageHandler;
-    }
-
-    @MessagingGateway(defaultRequestChannel = "mqttOutboundChannel")
-    public interface OutboundGateway {
-        void sendToMqtt(String payload, @Header(MqttHeaders.TOPIC) String topic);
-    }
 }
