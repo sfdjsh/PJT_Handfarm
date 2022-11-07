f54d85b87502d8327a5e647ec63b42b691b74ad8
유승우 <swyou1123@naver.com>
2022-11-07 16:43:03 +0900

    2022-11-1 commit

--- backend/src/main/java/com/handfarm/backend/sse/SseController.java
+++ backend/src/main/java/com/handfarm/backend/sse/SseController.java
@@ -12,13 +12,11 @@ import com.handfarm.backend.service.DeviceService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
+@RequestMapping("/api")
 @Slf4j
 public class SseController {
 
