9c36904f01133bbe5ec8ba644150dff7af6f803e
정석호 <sfdjsh@naver.com>
2022-11-08 17:59:13 +0900

    Feat: 내 농장 센서 통신 - 2

--- frontend/src/components/myFarm/SensorList.jsx
+++ frontend/src/components/myFarm/SensorList.jsx
@@ -1,59 +1,21 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { BASE_URL } from "../../config";
 import { useRecoilState } from "recoil";
-import { userFarm } from "../../atom";
-import { EventSourcePolyfill } from 'event-source-polyfill'
+import { deviceSensor } from "../../atom";
 
 const SensorList = ({ deviceId }) => {
-  const [myFarm, setMyFarm] = useRecoilState(userFarm)
-  const [count, setCount] = useState(null)
-
-  const handleConnect = () => {
-    console.log(deviceId)
-    const sse = new EventSourcePolyfill(`${BASE_URL}/connect/${deviceId}`)
-    sse.addEventListener('connect', (e) => {
-      const {data: receivedConnectData} = e;
-      console.log('connent event data:', receivedConnectData)
-    })
-    sse.CLOSED();
-    sse.addEventListener('count', e => {
-      const {data: receivedCount } = e;
-      console.log("count event data", receivedCount);
-      setCount(receivedCount)
-    })
-  }
-  // const sse = new EventSourcePolyfill(`${BASE_URL}/connect/${deviceId}`)
-  // sse.addEventListener('connect', (e) => {
-  //   const { data: receivedConnectData } = e;
-  //   setCount(receivedConnectData)
-  // })
-
-  // console.log(myFarm)
-  // axios({
-  //   url: `${BASE_URL}/farm/${deviceId}`,
-  //   method: "GET",
-  //   headers: {
-  //     accessToken: localStorage.getItem("access_token"),
-  //   },
-  // }).then((response) => {
-  //   console.log(response.data)
-  //   setMyFarm(response.data);
-  // }, []);
-  console.log(count)
-
-  // 데이터는 받는데.. 화면단에 보이는게 안됨...
+  const [sensor, setSensor] = useRecoilState(deviceSensor)
+  const zzz = [sensor[deviceId]]
   return (
     <div>
-      <button onClick={handleConnect}>connect 요청</button>
-      {/* <button onClick={handleCountClick}>count 요청</button> */}
-      {count}
-      {/* <div>
-        {count.humid}
-      </div>
-      <div>
-        {count.soilHumid}
-      </div> */}
+      {
+        zzz.map((wpqkf) => {
+          if (wpqkf.temp) {
+            <div>
+              {wpqkf.temp} 
+            </div>
+          }
+        })
+      }
     </div>
   )
 };
