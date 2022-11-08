d05b11c192892263f96e3a4e8a37c1310384baa7
정석호 <sfdjsh@naver.com>
2022-11-08 10:57:36 +0900

    Feat: 내 농장 센서 실시간 통신 - 1

--- frontend/src/components/myFarm/SensorList.jsx
+++ frontend/src/components/myFarm/SensorList.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { BASE_URL } from "../../config";
 import { useRecoilState } from "recoil";
@@ -9,29 +9,44 @@ const SensorList = ({ deviceId }) => {
   const [myFarm, setMyFarm] = useRecoilState(userFarm)
   const [count, setCount] = useState(null)
 
-  console.log(deviceId)
-  const sse = new EventSourcePolyfill(`${BASE_URL}/connect/${deviceId}`)
-  sse.addEventListener('connect', (e) => {
-    const { data: receivedConnectData } = e;
-    setCount(receivedConnectData) 
-  })
-  
+  const handleConnect = () => {
+    console.log(deviceId)
+    const sse = new EventSourcePolyfill(`${BASE_URL}/connect/${deviceId}`)
+    sse.addEventListener('connect', (e) => {
+      const {data: receivedConnectData} = e;
+      console.log('connent event data:', receivedConnectData)
+    })
+    sse.CLOSED();
+    sse.addEventListener('count', e => {
+      const {data: receivedCount } = e;
+      console.log("count event data", receivedCount);
+      setCount(receivedCount)
+    })
+  }
+  // const sse = new EventSourcePolyfill(`${BASE_URL}/connect/${deviceId}`)
+  // sse.addEventListener('connect', (e) => {
+  //   const { data: receivedConnectData } = e;
+  //   setCount(receivedConnectData)
+  // })
+
   // console.log(myFarm)
-      // axios({
-      //   url: `${BASE_URL}/farm/${deviceId}`,
-      //   method: "GET",
-      //   headers: {
-      //     accessToken: localStorage.getItem("access_token"),
-      //   },
-      // }).then((response) => {
-      //   console.log(response.data)
-      //   setMyFarm(response.data);
-      // }, []);
+  // axios({
+  //   url: `${BASE_URL}/farm/${deviceId}`,
+  //   method: "GET",
+  //   headers: {
+  //     accessToken: localStorage.getItem("access_token"),
+  //   },
+  // }).then((response) => {
+  //   console.log(response.data)
+  //   setMyFarm(response.data);
+  // }, []);
   console.log(count)
 
   // 데이터는 받는데.. 화면단에 보이는게 안됨...
   return (
     <div>
+      <button onClick={handleConnect}>connect 요청</button>
+      {/* <button onClick={handleCountClick}>count 요청</button> */}
       {count}
       {/* <div>
         {count.humid}
@@ -39,9 +54,6 @@ const SensorList = ({ deviceId }) => {
       <div>
         {count.soilHumid}
       </div> */}
-      {/* {count.map((c) => (
-        {c}
-      ))} */}
     </div>
   )
 };
