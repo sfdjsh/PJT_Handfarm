704f77bd282f61c442fc8a228eb751daf4d553b2
김강현 <kiki249049@gmail.com>
2022-11-14 15:57:18 +0900

    #11 feat : myPage & Farmmunity

--- frontend/src/pages/api/MyPage.js
+++ frontend/src/pages/api/MyPage.js
@@ -2,10 +2,43 @@ import { BASE_URL } from "../../config";
 import {LOCAL_URL} from "../../config";
 import axios from "axios";
 
-export async function fetchUserInfo() {
-    const URL = `${LOCAL_URL}/mypage`
+export async function fetchUserInfo(userNickname) {
+    console.log(userNickname)
+    const URL = `${BASE_URL}/mypage/${userNickname}`
     const response = await fetch(URL, {
         method : "GET",
+        headers : {
+            accessToken : localStorage.getItem("access_token")
+        }
+    })
+    return response
+}
+
+export async function changeOpen(isOpen) {
+    // if(isOpen){
+    //     isOpen = 1
+    // }else{
+    //     isOpen = 0
+    // }
+    console.log(isOpen)
+    const URL = `${BASE_URL}/mypage`
+    const response = await fetch(URL, {
+        method : "POST",
+        headers : {
+            accessToken : localStorage.getItem("access_token"),
+            "Content-Type" : "application/json",
+        },
+        body : JSON.stringify({
+            userOpen : isOpen,
+        })
+    })
+    return response
+}
+
+export async function updateUserInfo(userInfo) {
+    const URL = `${BASE_URL}/mypage`
+    const response = await fetch(URL, {
+        method : "PUT",
         headers : {
             accessToken : localStorage.getItem("access_token")
         }
