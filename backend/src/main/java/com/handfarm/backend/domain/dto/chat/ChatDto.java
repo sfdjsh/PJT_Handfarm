33b3c2257003832129e1263f438d3235d443218a
김혜진 <aa981204@naver.com>
2022-11-15 10:05:51 +0900

    feat : redis-chat

--- backend/src/main/java/com/handfarm/backend/domain/dto/chat/ChatDto.java
+++ backend/src/main/java/com/handfarm/backend/domain/dto/chat/ChatDto.java
@@ -14,5 +14,6 @@ public class ChatDto {
     private Integer roomId; // 각 구독 채널 구분할 수 있는 식별자
     private String toUserNickname; // 로그인 한 사람 닉네임
     private String sendUserNickname; // 받는사람닉네임
+    private String isRead; // 읽음 여부
     private String msg; // 메시지
 }
