217f17f494337577a1594abf7ad38ce947ebd03e
김도원 <da9065@naver.com>
2022-11-21 19:42:45 +0900

    update
    Former-commit-id: a181587b349843ec27d3004f1112ff97ef961044

--- ESP32s/NeoPixel_Test/NeoPixel_Test.ino
+++ ESP32s/NeoPixel_Test/NeoPixel_Test.ino
@@ -1,48 +1,96 @@
-#include <Adafruit_NeoPixel.h>
+#include <FastLED.h>               //FastLED 라이브러리 선언
 
-#define PIN 23
-#define STRIPSIZE 100
+#define DATA_PIN    23              //아두이노 6번핀을 "DATA_PIN"으로 지정
+#define LED_TYPE    WS2812         //WS2812(네오픽셀의 종류)를 "LED_TYPE"로 지정
+#define COLOR_ORDER GRB            //GRB(색상의 순서)를 "COLOR_ORDER"로 지정
+#define NUM_LEDS    80             //12(네오픽셀의 LED 개수)를 "NUM_LEDS"로 지정
+CRGB leds[NUM_LEDS];               
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(STRIPSIZE, PIN, NEO_GRB + NEO_KHZ800);
+#define BRIGHTNESS         255     //밝기지정 "BRIGHNESS" (0~255 값이 있으며 255 일 때 가장 밝다) 
+#define FRAMES_PER_SECOND  120     //120을 "FRAME_PER_SECOND"로 지정
 
 void setup() {
-  strip.begin();
-  strip.setBrightness(255);  // Lower brightness and save eyeballs!
-  strip.show(); // Initialize all pixels to 'off'
+  delay(3000);   //전원 안정화를 위한 딜레이
+  
+  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip); //<WS2812, 6, GRB> (leds, 12)
+   
+  FastLED.setBrightness(BRIGHTNESS);         //밝기 설정(255)
 }
 
-void loop() {
-  colorWave(50);
+typedef void (*SimplePatternList[])();
+SimplePatternList gPatterns = {bpm};
+
+uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
+uint8_t gHue = 0; // rotating "base color" used by many of the patterns
+  
+void loop()
+{
+   gPatterns[gCurrentPatternNumber]();
+
+  FastLED.show();  
+  FastLED.delay(1000/FRAMES_PER_SECOND); 
+
+  EVERY_N_MILLISECONDS( 20 ) { gHue++; } 
+  EVERY_N_SECONDS( 10 ) { nextPattern(); } 
 }
 
-void colorWave(uint8_t wait) {
-  int i, j, stripsize, cycle;
-  float ang, rsin, gsin, bsin, offset;
-  static int tick = 0;
-  stripsize = strip.numPixels();
-  cycle = stripsize * 25; // times around the circle...
 
-  while (++tick % cycle) {
-    offset = map2PI(tick);
-    for (i = 0; i < stripsize; i++) {
-      ang = map2PI(i) - offset;
-      rsin = sin(ang);
-      gsin = sin(2.0 * ang / 3.0 + map2PI(int(stripsize/6)));
-      bsin = sin(4.0 * ang / 5.0 + map2PI(int(stripsize/3)));
-      //strip.setPixelColor(i, strip.Color(trigScale(rsin), 0, 0));
-      strip.setPixelColor(i, strip.Color(trigScale(bsin), trigScale(bsin), trigScale(bsin)));
-    }
-    strip.show();
-    delay(wait);
+#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
+
+
+void nextPattern()
+{
+  // add one to the current pattern number, and wrap around at the end
+  gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
+}
+
+void rainbow() 
+{
+  fill_rainbow( leds, NUM_LEDS, gHue, 7);
+}
+
+void rainbowWithGlitter() 
+{
+  rainbow();
+  addGlitter(80);
+}
+
+void addGlitter( fract8 chanceOfGlitter) 
+{
+  if( random8() < chanceOfGlitter) {
+    leds[ random16(NUM_LEDS) ] += CRGB::White;
   }
 }
 
-byte trigScale(float val) {
-  val += 1.0; // move range to [0.0, 2.0]
-  val *= 127.0; // move range to [0.0, 254.0]
-
-  return int(val) & 255;
+void confetti() 
+{
+  fadeToBlackBy( leds, NUM_LEDS, 10);
+  int pos = random16(NUM_LEDS);
+  leds[pos] += CHSV( gHue + random8(64), 200, 255);
 }
-float map2PI(int i) {
-  return PI*2.0*float(i) / float(strip.numPixels());
+
+void sinelon()
+{
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  int pos = beatsin16( 13, 0, NUM_LEDS-1 );
+  leds[pos] += CHSV( gHue, 255, 192);
+}
+
+void bpm()
+{
+  uint8_t BeatsPerMinute = 62;
+  CRGBPalette16 palette = PartyColors_p;
+  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
+  for( int i = 0; i < NUM_LEDS; i++) { //9948
+    leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
+  }
+}
+
+void juggle() {
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  byte dothue = 0;
+  for( int i = 0; i < 8; i++) {
+    leds[beatsin16( i+7, 0, NUM_LEDS-1 )] |= CHSV(dothue, 200, 255);
+    dothue += 32;
+  }
 }
